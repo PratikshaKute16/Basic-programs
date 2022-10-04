591abe769bab1966e83a55ce0a21f217243991a2
Pratiksha Kute <pratikute1603@gmail.com>
2022-10-04 12:08:25 +0530

    Initial version

---
+++ src/SwappingTwoNum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class SwappingTwoNum {
+    public static void main(String[] args) {
+        int a=5,b=2,temp=0;
+        temp=a;
+        a=b;
+        b=temp;
+      System.out.println(a);
+      System.out.println(b);
+    }
+}
