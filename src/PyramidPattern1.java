4ca1ec3b57dc55141381c389b7ae6ce7f12fd7dd
Pratiksha Kute <pratikute1603@gmail.com>
2022-10-17 18:23:38 +0530

    Initial version

--- src/PyramidPattern1.java
+++ src/PyramidPattern1.java
@@ -3,7 +3,7 @@ public class PyramidPattern1 {
         for (int i = 1; i <= 5; i++) {
             for (int j = 4; j >= i; j--){
                 System.out.print(" ");}
-            for (int k = 1; k <= i; k++) {
+            for (int k = 1; k <=i; k++) {
                 System.out.print("*");
             }
             for (int l =2; l<= i; l++) {
