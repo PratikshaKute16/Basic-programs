b29387e58ec0f6428a1d2b698d8b681e814fbd75
Pratiksha Kute <pratikute1603@gmail.com>
2022-10-18 18:00:06 +0530

    Initial version

---
+++ src/hollowPyramid.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class hollowPyramid {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for (int k=1;k<=7;k++) {
+                if (i + k == 5|| k-i==3 ||i==4) {
+                    System.out.print("*");
+                }else
+                    System.out.print(" ");
+            }
+            System.out.println(  );
+        }
+    }
+}
