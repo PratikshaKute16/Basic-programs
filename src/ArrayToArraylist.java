54a84b364b6ebbed49731d0a065202619f1fb053
Pratiksha Kute <pratikute1603@gmail.com>
2022-11-07 11:49:20 +0530

    Initial version

--- src/ArrayToArraylist.java
+++ src/ArrayToArraylist.java
@@ -6,7 +6,11 @@ public class ArrayToArraylist {
 //        List list= Arrays.asList(s);
 //        System.out.println(list);
         List<String> l= new ArrayList<>();
-        Collections.addAll(l,s);
+//        Collections.addAll(l,s);
+//        System.out.println(l);
+        for(String string:s){
+            l.add(string);
+        }
         System.out.println(l);
 
 
