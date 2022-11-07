69ab2ca7c3abfe9e8e181589bd1775e2076e9ba9
Pratiksha Kute <pratikute1603@gmail.com>
2022-11-07 11:44:12 +0530

    Initial version

---
+++ src/ArrayToArraylist.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayToArraylist {
+    public static void main(String[] args) {
+        String[] s={"Apple","Ball","Cat"};
+        List list= Arrays.asList(s);
+        System.out.println(list);
+
+
+    }
+}
