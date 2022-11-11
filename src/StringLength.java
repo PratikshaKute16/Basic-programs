c7869dd1fc83f0833a9b9ecb1f3a5e797f9f0bcc
Pratiksha Kute <pratikute1603@gmail.com>
2022-11-11 14:58:57 +0530

    Initial version

--- src/StringLength.java
+++ src/StringLength.java
@@ -1,6 +1,18 @@
 public class StringLength {
     public static void main(String[] args) {
-        String s="Hello World";
-        System.out.println(s.length());
+      String s="Hello World";
+      //By using inbuild method
+//        System.out.println(s.length());
+        //By without inbuild method
+        s=s.concat("\0");
+        char a[]=s.toCharArray();
+        int count=0;
+        int i=0;
+        while(a[i]!=0){
+            ++count;
+            ++i;
+        }
+        System.out.println(count);
+
     }
 }
